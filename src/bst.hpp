74d2e8b0c76b1a1607e6a8f4b417eaf9615347bc
Christian Altamirano <bdiehs@mit.edu>
2021-03-15 19:06:41 -0400

    Added BST nodes and rotation method

--- src/bst.hpp
+++ src/bst.hpp
@@ -1,7 +1,21 @@
 template <typename K, typename V>
+class BSTNode {
+public:
+	BSTNode(K key, V val, BSTNode* parent);
+	void rotate();
+	K key;
+	V val;
+	BSTNode* left;
+	BSTNode* right;
+	BSTNode* parent;
+};
+
+template <typename K, typename V>
 class BST {
-    public:
+public:
+	BST() : root(nullptr) {}
     void insert(K key, V val) = 0;
     void remove(K key) = 0;
     V find(K key) = 0;
+	BSTNode<K, V>* root;
 };
